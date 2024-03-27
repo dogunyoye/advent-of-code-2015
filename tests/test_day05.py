2bc6aed802caf921e44bc77b976e478b360cde26
Dimeji <dimejiogunyoye@googlemail.com>
2024-03-27 02:21:37 +0000

    Day 05 - remove comment in test

--- tests/test_day05.py
+++ tests/test_day05.py
@@ -5,7 +5,7 @@ from day05 import day05
 
 class Day05Test(unittest.TestCase):
     def test_part_one(self):
-        self.assertEqual(day05.find_number_of_nice_strings("ugknbfddgicrmopn"), 1)  # add assertion here
+        self.assertEqual(day05.find_number_of_nice_strings("ugknbfddgicrmopn"), 1)
 
     def test_part_two(self):
         self.assertEqual(day05.find_number_of_nice_strings_with_new_rules("qjhvhtzxzqqjkmpb"), 1)
