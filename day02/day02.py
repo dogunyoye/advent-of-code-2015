b84fa0b10ab3d0eb76bb325eff8fec0cf7bb8b4a
Dimeji <dimejiogunyoye@googlemail.com>
2024-03-28 01:03:28 +0000

    Day 02 - minor refactor

--- day02/day02.py
+++ day02/day02.py
@@ -8,14 +8,8 @@ def calculate_total_square_feet(data) -> int:
     result = 0
     for line in data.splitlines():
         dimensions = [eval(i) for i in re.findall(r'(\d+)x(\d+)x(\d+)', line)[0]]
-        length = dimensions[0]
-        width = dimensions[1]
-        height = dimensions[2]
-
-        lw = length * width
-        wh = width * height
-        hl = height * length
-
+        length, width, height = dimensions[0], dimensions[1], dimensions[2]
+        lw, wh, hl = length * width, width * height, height * length
         result += ((2 * lw) + (2 * wh) + (2 * hl) + min(lw, wh, hl))
     return result
 
@@ -24,13 +18,10 @@ def calculate_feet_of_ribbon(data) -> int:
     result = 0
     for line in data.splitlines():
         dimensions = [eval(i) for i in re.findall(r'(\d+)x(\d+)x(\d+)', line)[0]]
-        length = dimensions[0]
-        width = dimensions[1]
-        height = dimensions[2]
+        length, width, height = dimensions[0], dimensions[1], dimensions[2]
 
         present = min((2 * length + 2 * width), (2 * width + 2 * height), (2 * length + 2 * height))
         bow = length * width * height
-
         result += bow + present
     return result
 
