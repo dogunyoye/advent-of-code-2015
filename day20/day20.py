20785c7b1e418da18acde4c620977f424b15b1dc
Dimeji <dimejiogunyoye@googlemail.com>
2024-04-05 01:20:37 +0100

    Day 20 - part 2

--- day20/day20.py
+++ day20/day20.py
@@ -24,10 +24,34 @@ def find_lowest_house_number_to_receive_36000000_presents(data) -> int:
         house_number += 1
 
 
+def find_lowest_house_number_to_receive_36000000_presents_with_constraints(data) -> int:
+    house_number = 1
+    total_presents = eval(data)
+
+    while True:
+        factors = {1}
+        dividend, div = house_number, 1
+        quotient = dividend // div
+
+        while quotient > div:
+            if dividend % div == 0:
+                factors.add(quotient)
+                factors.add(div)
+            div += 1
+            quotient = dividend // div
+
+        factors = list(filter(lambda f: (house_number // f) <= 50, factors))
+        if sum(list(map(lambda f: f * 11, factors))) >= total_presents:
+            return house_number
+        house_number += 1
+
+
+# TODO slow solution, optimisation needed
 def main() -> int:
     with open(DATA) as f:
         data = f.read()
         print("Part 1: " + str(find_lowest_house_number_to_receive_36000000_presents(data)))
+        print("Part 2: " + str(find_lowest_house_number_to_receive_36000000_presents_with_constraints(data)))
     return 0
 
 
